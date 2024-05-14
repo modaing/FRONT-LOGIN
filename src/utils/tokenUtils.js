7eb1802e58beffdf81dc1f797f75dd1e3221bae1
lja9207 <3wksu2@hanmail.net>
2024-05-14 18:30:23 +0900

    feat#1 전자결재 상신함 조회

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -1,8 +1,15 @@
 import jwt_decode from "jwt-decode";
 
 export function decodeJwt(token) {
+    try {
+        // 토큰이 없는 경우 또는 null인 경우
+        if (!token) return null;
 
-    if(token === null) return null;
-
-    return jwt_decode(token);
-};
+        // 토큰 디코딩
+        return jwt_decode(token);
+    } catch (error) {
+        // 토큰 디코딩 중 오류가 발생한 경우
+        console.error("Error decoding token:", error);
+        return null;
+    }
+}
