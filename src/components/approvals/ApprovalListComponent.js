79db2267f9eb759c781ab7a9cd5f8f35d949c4dc
lja9207 <3wksu2@hanmail.net>
2024-05-16 11:17:31 +0900

    feat#1 전자결재 상신함 목록 조회

--- src/components/approvals/ApprovalListComponent.js
+++ src/components/approvals/ApprovalListComponent.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 import ApprovalAPI from "../../apis/ApprovalAPI";
 import { decodeJwt } from "../../utils/tokenUtils";
+import styles from '../../css/approval/ApprovalListComponent.module.css';
 
-function ApprovalListComponent(){
+function ApprovalListComponent() {
     const [approvals, setApprovals] = useState(null);
     const [loading, setLoading] = useState(true);
     const [pageInfo, setPageInfo] = useState({});
+    const [fg, setFg] = useState('given');
+    const [page, setPage] = useState(0);
+    const [title, setTitle] = useState('');
+    const [direction, setDirection] = useState('DESC');
 
-    
+
     const decodedToken = decodeJwt(window.localStorage.getItem('accessToken'));
     console.log('[SendApprovalList] decodedToken : ', decodedToken);
     const memberId = decodedToken.memberId;
     console.log('[SendApprovalList] memberId : ' + memberId);
 
     useEffect(() => {
-        ApprovalAPI.getApprovals().then((res) =>{
+        ApprovalAPI.getApprovals().then((res) => {
             // setApprovals(res.data?.data);
             console.log('approvals data: ' + JSON.stringify(res.data, null, 2));
             setApprovals(res.data.data?.content);
@@ -26,14 +31,9 @@ function ApprovalListComponent(){
         });
     }, []);
 
-    if(loading){
-        return <div>Loading...</div>
-    }
-
-
-    return(
-        <div>
-            <table>
+    return (
+        <div className={styles.tableContainer}>
+            <table className={styles.table}>
                 <thead>
                     <tr>
                         <th>기안 일시</th>
@@ -43,15 +43,32 @@ function ApprovalListComponent(){
                     </tr>
                 </thead>
                 <tbody>
-                    {approvals.map(approval =>
-                        <tr key={approval.approvalNo}>
-                            <td>{approval.approvalDate}</td>
-                            <td>{approval.approvalformName}</td>
-                            <td>{approval.approvalTitle}</td>
-                            <td>{approval.approvalStatus}</td>
+                    {loading ? (
+                        <tr>
+                            <td colSpan="4" className={styles.loadingMessage}>전자결재 목록을 불러오는 중입니다...</td>
                         </tr>
+                    ) : (
+                        approvals.length > 0 ? (
+                            approvals.map(approval =>
+                                <tr key={approval.approvalNo}>
+                                    <td>{approval.approvalDate}</td>
+                                    <td>{approval.formName}</td>
+                                    <td>{approval.approvalTitle}</td>
+                                    <td>{approval.approvalStatus}</td>
+                                </tr>
+                            )
+                        ) : (
+                            <tr>
+                                <td colSpan="4" className={styles.loadingMessage}>등록된 전자결재가 없습니다.</td>
+                            </tr>
+                        )
+
                     )}
+                    <tr>
+                        <td colSpan="4" className={styles.loadingMessage}>등록된 전자결재가 없습니다.</td>
+                    </tr>
                 </tbody>
+
             </table>
         </div>
     );
--- src/pages/approval/ApprovalList.js
+++ src/pages/approval/ApprovalList.js
@@ -1,20 +1,78 @@
-import { Switch, Route, Router } from 'react-router-dom';
+import { Link, Route, Router } from 'react-router-dom';
 import ApprovalListComponent from '../../components/approvals/ApprovalListComponent';
+import '../../css/approval/approval.css';
 
 function ApprovalList() {
 
-    
+    const sendAppListButton = {
+        height: '40px',
+        width: '120px',
+        backgroundColor: '#112D4E',
+        color: 'white',
+        borderRadius: '5px',
+        padding: '1% 1.5%',
+        cursor: 'pointer',
+        textDecoration: 'none',
+        marginRight: '20px',
+        verticalAlign: 'middle',
+        textAlign: 'center',
+        display: 'inline-block',
+        paddingTop: '7px'
+    };
+
+    const tempAppListButton = {
+        height: '40px',
+        width: '120px',
+        backgroundColor: 'white',
+        color: '#112D4E',
+        borderRadius: '5px',
+        border: '1px solid #D5D5D5',
+        padding: '1% 1.5%',
+        cursor: 'pointer',
+        textDecoration: 'none',
+        verticalAlign: 'middle',
+        textAlign: 'center',
+        display: 'inline-block',
+        paddingTop: '7px'
+
+    }
+
 
     return (
         <>
-           
-                <main id="main" className="main">
-                    <div className="pagetitle">
-                        <h1>결재 상신함</h1>
-                        <ApprovalListComponent/>
-                    </div>
-                </main>
-            
+
+            <main id="main" className="main">
+                <div className="pagetitle">
+                    <h1>결재 상신함</h1>
+                    <nav>
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item"><a href="/">Home</a></li>
+                            <li className="breadcrumb-item">전자결재</li>
+                            <li className="breadcrumb-item active">결재 상신 내역</li>
+                        </ol>
+                        <div className="approvalBar">
+                            <div className="approvalBarLeft">
+                                <Link to="/ApprovalList" className="ApprovalListBtn" style={sendAppListButton}>내 결재함</Link>
+                                <Link to="/tempApprovalList" className="ApprovalListBtn" style={tempAppListButton}>임시 저장함</Link>
+                            </div>
+                            <div className="approvalBarRight">
+                                <div className="approvalSearch">
+                                    <div className="approvalSearchLabel">제목</div>
+                                    <div className="approvalSearchInput">
+                                        <input type="text"></input>
+                                    </div>
+                                    <div className="approvalSearchBtn">
+                                        <button type="submit">검색</button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                    <ApprovalListComponent />
+                </div>
+            </main>
+
         </>
     )
 }
