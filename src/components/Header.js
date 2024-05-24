c44895def591c93db8f4ae6f4b43a3107091098b
jeonghee <hongi_@naver.com>
2024-05-25 05:46:53 +0900

    feat #18 - [출퇴근] 부서별 출퇴근 내역 조회

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,12 +40,13 @@ function Header() {
     };
 
     return (
-        <header id="header" className="header fixed-top d-flex align-items-center">
+        <header id="header" className="header fixed-top d-flex align-items-center" >
             <div className="d-flex align-items-center justify-content-between">
                 {/* 홈으로 이동하는 링크 */}
                 <Link to="/" className="logo d-flex align-items-center">
                     <img src="img/logo.png" alt="" />
                 </Link>
+                <i class="bi bi-list toggle-sidebar-btn"></i>
             </div>
             <nav className="header-nav ms-auto">
                 <ul className="d-flex align-items-center">
