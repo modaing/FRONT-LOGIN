293ee431d0aa57b3a7ef7b532aaa9d7b0ee1921e
lja9207 <3wksu2@hanmail.net>
2024-05-30 11:14:40 +0900

    style#2 전자결재 페이징 통합

--- src/components/approvals/Pagination.js
+++ src/components/approvals/Pagination.js
@@ -1,22 +1,23 @@
 import React, { useCallback } from 'react';
-import styles from '../../css/approval/ApprovalListComponent.module.css'
+// import styles from '../../css/approval/ApprovalListComponent.module.css'
+import styles from '../../css/approval/Pagination.module.css';
 
 function Pagination({ totalPages, currentPage, onPageChange }) {
-    
+
     console.log(`현재 페이지 : ${currentPage}`);
     console.log(`총 페이지 : ${totalPages}`);
 
     const handleClick = useCallback((pageNumber, event) => {
 
-        if(event){
+        if (event) {
             event.stopPropagation();
         }
-        
+
         onPageChange(pageNumber);
     }, [onPageChange]);
 
     const buttonStyle = () => {
-        return {marginLeft : '0px'};
+        return { marginLeft: '0px' };
     }
 
     const getPaginationRange = () => {
@@ -36,6 +37,9 @@ function Pagination({ totalPages, currentPage, onPageChange }) {
             end = totalPages;
         }
 
+        start = Math.max(start, 0);
+        end = Math.min(end, totalPages);
+
         return Array.from({ length: end - start }, (_, i) => i + start);
     };
 
@@ -43,39 +47,63 @@ function Pagination({ totalPages, currentPage, onPageChange }) {
 
 
     return (
-        <div className='pagination approvalPage'>
-            
-            <button
-                className='page-link'
-                disabled={currentPage === 0}
-                onClick={(event) => handleClick(currentPage - 1, event)}
-            >
-                {'<<'}
-            </button>
-            <div className={styles.pagination}>
-                {Array.from({ length: totalPages }, (_, index) => (
-                    <button
-                        key={index}
-                        className={`${styles.approvalButton} ${index === currentPage ? styles.activePage : ''}`}
-                        disabled={index === currentPage}
-                        onClick={(event) => handleClick(index, event)}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
-            </div>
-            <button
-                className='page-link'
-                disabled={currentPage === totalPages - 1}
-                onClick={(event) => handleClick(currentPage + 1, event)}
-                style={buttonStyle()}
-            >
-                {'>>'}
-            </button>
+        // <div className='pagination approvalPage'>
 
-            
+        //     <button
+        //         className='page-link'
+        //         disabled={currentPage === 0}
+        //         onClick={(event) => handleClick(currentPage - 1, event)}
+        //     >
+        //         ◀
+        //     </button>
+        //     <div className="pagination">
+        //         {Array.from({ length: totalPages }, (_, index) => (
+        //             <button
+        //                 key={index}
+        //                 className={`"approvalButton" ${index === currentPage ? 'activePage' : ''}`}
+        //                 disabled={index === currentPage}
+        //                 onClick={(event) => handleClick(index, event)}
+        //             >
+        //                 {index + 1}
+        //             </button>
+        //         ))}
+        //     </div>
+        //     <button
+        //         className='page-link'
+        //         disabled={currentPage === totalPages - 1}
+        //         onClick={(event) => handleClick(currentPage + 1, event)}
+        //         style={buttonStyle()}
+        //     >
+        //         ▶
+        //     </button>
 
-        </div>
+
+
+        // </div>
+        <nav>
+            <ul className='pagination'>
+            <li className={`page-item ${currentPage === 0 && 'disabled'}`}>
+                  <button className='page-link' onClick={(event) => handleClick(currentPage - 1, event)} disabled={currentPage === 0} >◀</button>
+                </li>
+                {paginationRange.length === 0 ? (
+                    <li className='page-item'>
+                        <button className='page-link' disabled>1</button>
+                    </li>
+                ) :  (
+                    paginationRange.map(page => (
+                        <li key={page} className={`page-item ${currentPage === page && 'active'}`}>
+                            <button className='page-link' onClick={(event) => handleClick(page, event)}>
+                                {page + 1}
+                            </button>
+                        </li>
+                    ))
+            )}
+                <li className={`page-item ${currentPage === totalPages - 1 && 'disabled'}`}>
+                    <button className='page-link' onClick={(event) => handleClick(currentPage + 1, event)} disabled={currentPage === totalPages - 1}>▶</button>
+                </li>
+
+            </ul>
+        </nav>
     )
 };
 
